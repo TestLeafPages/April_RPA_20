83e0a5e40f8a44a38a20942e36cc99a9d72ee642
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-05-26 09:28:27 +0530

    Add files via upload

---
+++ week5/learnCSharp/LeanrnRegEX.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace AprLearnCSharp
+{
+    class LeanrnRegEX
+    {         //OutPut                //Input for method
+              //AM returnType   MethodName(Arg)
+              //no output
+
+        static void Main(string[] args)
+        {
+           // LeanrnRegEX re = new LeanrnRegEX();
+            reGex();
+        }
+
+
+            public static void reGex()
+        {
+            string text = "UiPath Certification 2020 and _Sarath@gmail.com more than 35000 got certified last year across globe";
+            string pat = "[a-zA-Z]+[@][a-z]+[.][a-z]{3}";
+
+            MatchCollection matchCollection = Regex.Matches(text, pat);
+            foreach(Match eachMatch in matchCollection)
+            {
+                Console.WriteLine(eachMatch);
+            }
+           
+
+
+
+            /* [abc] -> ex -> 'd' => false
+             * [a-zA-Z]  -> ex -> 'e' or 'E' => true
+             * [0-9] - > ex -> 5 => true
+             *  \d  -> ex -> 5 => true
+             *  ^[0-9] or \D ex -> '@' => true
+             *  ^[abc] ex -> 'A' => true
+             *  \w (only words=a-zA-Z0-9)
+             *  \W (not words=a-zA-Z0-9)
+             *  [ ] or \s -> ex-> ' ' => True
+             *  \S ex-> ' ' => false
+             *  
+             *  {n}(if know the count) ->  ex ->[a-z]{3} "abc"  => true
+             *  {n,m}(min-count, max-count)  ->ex ->[a-z]{3,15} "certification"  => true
+             *  {n,}(min-Value, ~)  ->ex ->[a-z]{3,} "certification"  => true
+             *  * -> 0 or more
+             *  ? -> 0 or 1
+             *  + -> 1 or more
+             *  
+*/        }
+    }
+}
---
+++ week5/learnCSharp/LearnStrings.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprLearnCSharp // Package or subFolder of project
+{
+    class LearnStrings
+    //NameSpace - > using /   import(java) -> package
+        //Class - > Car
+            //methods
+            //variable - > info about the class -> model,color,no of seats
+    {
+        static void Main(string[] args) // -> main Method   -> Main.xaml
+        {
+            
+            // dataType nameOfVariable = value;
+            string text1 = " Learn RPA is Fun ";
+            string text2 = "RPA";// ascii -> a -> 65
+            string[] allWords = text1.Split(' ');
+            Console.WriteLine(allWords.Length);
+            string joinedText = string.Join("",allWords);
+            /*Console.WriteLine(joinedText.ToLower());
+            Console.WriteLine(joinedText.ToUpper());*/
+            Console.WriteLine(text1.Replace("[a-z]", "CSharp"));
+            Console.WriteLine(text1.Insert(0, "The "));
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*foreach(string eachItem in allWords)
+            {
+                Console.WriteLine(eachItem);
+            }*/
+            // if (text1.Equals(text2))
+            //(text1.Equals(text2,StringComparison.InvariantCultureIgnoreCase))
+            /* if(text1.Contains(text2))
+             {
+                 Console.WriteLine("Matching");
+             }
+             else
+             {
+                 Console.WriteLine("not Matching");
+             }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // int charCount = text.Length;
+            //className objectName = new className(); get Access for the class
+            //objectName.methodName();
+            //ClassName.Method - > Static Way
+            // Console.WriteLine(charCount);
+            /*for(int i=0; i < text.Length; i++)
+            {
+                Console.Write(text[i]);
+            }
+            Console.WriteLine();*/
+
+
+
+
+
+        }
+    }
+}
